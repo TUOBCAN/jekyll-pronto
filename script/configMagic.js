075a1ae70cdde7a8b1219fd9cdab3e6552093bf3
PressJump <TUOBCAN@users.noreply.github.com>
2021-07-23 20:25:13 +1000

    ✨Download Page & Wiki Links
    - Added optional Download page for project websites
    - Edited 404 Page and added a random joke using icanhazdadjoke API which can be found at https://icanhazdadjoke.com/
    - Added Wiki Links to be used with Markdown
    - Added GitHub Actions for "Config Magic" as trickypr says
    
    Co-Authored-By: trickypr <23250792+trickypr@users.noreply.github.com>

---
+++ script/configMagic.js
@@ -0,0 +1,26 @@
+let config = readFileSync("./_config.yml");
+
+String.prototype.replaceBetween = function (start, end, what) {
+  return this.substring(0, start) + what + this.substring(end);
+};
+
+const exec = require("child_process").exec;
+function execute(command, callback) {
+  exec(command, function (error, stdout, stderr) {
+    callback(stdout);
+  });
+}
+
+const repoKeyIndex = config.indexOf("repo:");
+if (repoKeyIndex !== -1) {
+  const newLine = config.indexOf("\n", repoKeyIndex);
+
+  execute("git config --get remote.origin.url", (url) => {
+    config.replaceBetween(
+      repoKeyIndex,
+      newLine,
+      url.replace("https://github.com/", "") + "\n"
+    );
+    writeFileSync("./_config.yml", config);
+  });
+}
